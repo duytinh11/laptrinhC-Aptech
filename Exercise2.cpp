d98527b41ef350426553e40ca4d2eda2b2982730
Tj <duytinh.vn@gmail.com>
2025-07-03 17:56:29 +0700

    Add files via upload

---
+++ Exercise2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int isPrime(int n){
+	
+}
+
+int main(){
+	int n;
+	printf("nhap so nguyen duong n co phai so nguyen to khong \n");
+	do{
+		printf("nhap n= ");
+		scanf("%d",&n);
+		if(n<=0)printf("n>0 vui long nhap lai\n");
+	}while(n<=0);
+	
+}
